ad832268960dffb19ecc8974c0f0f766a1fb4835
shtoneyan <tikinbobo@gmail.com>
2022-02-12 08:46:12 -0500

    remove junk losses

--- scripts/loss.py
+++ scripts/loss.py
@@ -3,28 +3,6 @@ import tensorflow_probability as tfp
 import numpy as np
 from util import bin_resolution
 
-class zero_infl_poiss(tf.keras.losses.Loss):
-    def __init__(self, name="zero_infl_poiss", **kwargs):
-        super().__init__(name=name)
-
-    def call(self, y_true, y_pred):
-        tfd = tfp.distributions
-        #multinomial part of loss function
-        rate = tf.math.exp(y_pred)
-        dim =y_true.shape
-        nonzero_prob = tf.math.divide(tf.cast(
-                                     tf.math.count_nonzero(y_pred, axis=(1, 2)), tf.float32),
-                                     tf.cast(tf.size(y_pred), tf.float32))
-        nonzero_prob = tf.expand_dims(nonzero_prob,axis =0)
-        nonzero_prob=tf.tile(tf.transpose(nonzero_prob),[1,int(dim[1]*dim[2])])
-        nonzero_prob = tf.reshape(nonzero_prob,dim)
-        cat = tfd.Categorical(probs=tf.stack([1-nonzero_prob, nonzero_prob], -1))
-        components = [tfd.Deterministic(loc=tf.zeros_like(rate)), tfd.Poisson(rate)]
-        zip_dist = tfd.Mixture(cat=cat, components=components)
-        # get the sequence length for normalization
-        seqlen = tf.cast(dim[0],dtype=tf.float32)
-        zip_loss = -tf.reduce_sum(zip_dist.log_prob(y_true)) / seqlen
-        return zip_loss
 
 class pearsonr_mse(tf.keras.losses.Loss):
     def __init__(self, name="pearsonr_mse", **kwargs):
@@ -60,50 +38,12 @@ class pearsonr_poisson(tf.keras.losses.Loss):
         total_loss = (2*pr*poiss)/(pr+poiss)
         return total_loss
 
-class fft_mse(tf.keras.losses.Loss):
-    def __init__(self, name="fftmse", **kwargs):
-        super().__init__(name=name)
-
-    def call(self, y_true, y_pred):
-
-        y_true = tf.cast(y_true, 'complex64')
-        y_pred = tf.cast(y_pred, 'complex64')
-        N, L, T = y_true.shape
-        fft_y = tf.math.abs(tf.slice(tf.signal.fft(y_true),[0,0,0], [N, L//2, T]))
-        fft_pred = tf.math.abs(tf.slice(tf.signal.fft(y_pred),[0,0,0], [N, L//2, T]))
-        fft_diff = tf.math.subtract(fft_y, fft_pred)
-        fft_diff = tf.dtypes.cast(fft_diff, 'float32')
-        return tf.math.square(fft_diff)
-
-
 class poisson(tf.keras.losses.Loss):
     def __init__(self, name="poisson", **kwargs):
         super().__init__(name=name)
 
     def call(self, y_true, y_pred):
         return tf.keras.losses.poisson(y_true, y_pred)
-
-class log_poisson(tf.keras.losses.Loss):
-    def __init__(self, name="log_poisson", **kwargs):
-        super().__init__(name=name)
-
-    def call(self, y_true, y_pred):
-        y_true = tf.math.log(y_true+1)
-        y_pred = tf.math.log(y_pred+1)
-        return tf.keras.losses.poisson(y_true, y_pred)
-
-class fftabs(tf.keras.losses.Loss):
-    def __init__(self, name="fftabs", **kwargs):
-        super().__init__(name=name)
-
-    def call(self, y_true, y_pred):
-        y_true = tf.cast(y_true, 'complex64')
-        y_pred = tf.cast(y_pred, 'complex64')
-
-        fft_y = tf.signal.fft(y_true)
-        fft_pred = tf.signal.fft(y_pred)
-        fft_diff = tf.math.subtract(fft_y, fft_pred)
-        return tf.math.abs(fft_diff)
 
 class mse(tf.keras.losses.Loss):
     def __init__(self, name="mse", **kwargs):
@@ -165,36 +105,6 @@ class multinomialnll_mse_reg(tf.keras.losses.Loss):
 
         #sum with weight
         total_loss = self.alpha*mult_loss + mse_loss
-
-        return total_loss
-
-
-class log_multinomialnll_mse(tf.keras.losses.Loss):
-    def __init__(self, name="multinomial", **kwargs):
-        super().__init__(name=name)
-
-    def call(self, y_true, y_pred, true_cov, pred_cov):
-        #do log on all inputs
-        y_true = tf.math.log(y_true)
-        y_pred = tf.math.log(y_pred)
-        true_cov = tf.math.log(true_cov)
-        pred_cov = tf.math.log(pred_cov)
-
-        #multinomial part of loss function
-        logits_perm = tf.transpose(y_pred, (0, 2, 1))
-        true_counts_perm = tf.transpose(y_true, (0, 2, 1))
-        counts_per_example = tf.reduce_sum(true_counts_perm, axis=-1)
-        dist = tfp.distributions.Multinomial(total_count=counts_per_example,
-                                                logits=logits_perm)
-        # get the sequence length for normalization
-        seqlen = tf.cast(tf.shape(y_true)[0],dtype=tf.float32)
-        mult_loss = -tf.reduce_sum(dist.log_prob(true_counts_perm)) / seqlen
-
-        #MSE part of loss function
-        mse_loss = tf.keras.losses.MSE(true_cov, pred_cov)
-
-        #sum with weight
-        total_loss = mult_loss + 10*mse_loss
 
         return total_loss
 
@@ -260,31 +170,3 @@ class r2 (tf.keras.losses.Loss):
 
         r2 = tf.ones_like(shape, dtype=tf.float32) - tf.divide(resid, total)
         return -tf.reduce_mean(r2)
-
-
-def logclass(func):
-    def wrapper(y_true,y_pred):
-        y_true = tf.math.log(y_true+1)
-        # y_pred = tf.math.log(y_pred+1)
-        return func(y_true,y_pred)
-    return wrapper
-
-
-class multiscale_poisson(tf.keras.losses.Loss):
-
-    def __init__(self, name="multiscale_poisson", **kwargs):
-        super().__init__(name=name)
-        # self.loss_fn = eval(kwargs.get('loss_fn'))()
-        self.loss_fn = eval('poisson')()
-        self.bin_sizes, self.alpha_weights = kwargs.get('loss_params')
-        # self.bin_sizes, self.alpha_weights = [[1], [1]]
-        # self.alpha_weights = kwargs.get('alpha_values')
-
-    def call(self, y_true, y_pred):
-        loss_values = []
-        for i, bin_size in enumerate(self.bin_sizes):
-            y_true_binned = bin_resolution(y_true, bin_size)
-            y_pred_binned = bin_resolution(y_pred, bin_size)
-            bin_loss = self.loss_fn(y_true_binned, y_pred_binned)
-            loss_values.append(self.alpha_weights[i]*bin_loss)
-        return loss_values
