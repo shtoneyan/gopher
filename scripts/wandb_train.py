33f97418ed7985ad51b6eec1dd6f3331ad2b3c38
amberT15 <amber.zqt@gmail.com>
2022-03-31 10:46:42 -0400

    add default learning rate

--- scripts/wandb_train.py
+++ scripts/wandb_train.py
@@ -135,7 +135,7 @@ def fit_robust(model_name_str, loss_type_str, window_size, bin_size, data_dir,
 
 def train_binary(model_name_str,data_dir,window_size,output_dir,config = {}):
     default_config = {'num_epochs': 30, 'batch_size': 64,
-                      'es_patience': 10, 'verbose' : True,
+                      'es_patience': 10, 'verbose' : True,'l_rate':0.001,
                       'lr_patience': 3, 'lr_decay': 0.2,
                       'loss_fn': 'BinaryCrossentrophy','log_wandb':True}
 
