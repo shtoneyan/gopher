9970ef23ba461b3aeb95bcfd6e8fc34f7397416f
Shushalsec <tikinbobo@gmail.com>
2022-04-29 18:07:44 -0400

    fix bpnet paths

--- analysis/performance_across_bin_sizes.py
+++ analysis/performance_across_bin_sizes.py
@@ -1,28 +1,32 @@
 import evaluate
 import pandas as pd
 import sys
+import glob
 sys.path.append('../gopher')
 import utils
 import numpy as np
 import json
 
 
+def get_runs(glob_pattern):
+    bin_run = {}
+    for run_dir in glob.glob(glob_pattern):
+        config = utils.get_config(run_dir)
+        if config['loss_fn']['value'] == 'poisson':
+            bin_run[config['bin_size']['value']] = run_dir
+    return bin_run
+
 result_base_dir = utils.make_dir('inter_results')
 # get datasets
 testset, targets = utils.collect_whole_testset('../datasets/quantitative_data/testset/')
 
-model_labels = ['binloss_basenji_v2', 'bpnet_bin_loss_40']
-for model_label in model_labels:
-    summary_performance = pd.read_csv(result_base_dir+'/model_evaluations/{}.csv'.format(model_label))
-
-    poisson_df = summary_performance[(summary_performance['targets'] == 'PC-3') &
-                                     (summary_performance['loss_fn'] == 'poisson')]
-
-    bin_run = dict(zip(poisson_df['bin_size'].values, poisson_df['run_dir'].values))
+model_run_pattern = {'binloss_basenji_v2': '../trained_models/basenji_v2/binloss_basenji_v2/run*',
+                     'bpnet_bin_loss_40': '../trained_models/bpnet/bin_loss_40/run*'}
+for model_label, run_pattern in model_run_pattern.items():
+    bin_run = get_runs(run_pattern)
     # get performance metrics for various evaluation bin sizes
-    result_path = result_base_dir+'/{}_triangle_plot.txt'.format(model_label)
+    result_path = result_base_dir + '/{}_triangle_plot.txt'.format(model_label)
     bin_sizes = sorted(list(bin_run.keys()))
-
     performance_per_resolution = []
     for raw_bin_size in bin_sizes:
         model, _ = utils.read_model(bin_run[raw_bin_size])
